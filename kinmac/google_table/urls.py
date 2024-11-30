c27b98cd8fa1d830c9c4ca9fb8e541e4ddd33db7
anton1202-py <anton1202@yandex.ru>
2024-11-30 20:34:10 +0300

    Общая таблица с данными

--- kinmac/google_table/urls.py
+++ kinmac/google_table/urls.py
@@ -1,12 +1,14 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from google_table.views import ResponseWithAllViewSet
+from google_table.views import ActionArticleViewSet, ResponseWithAllViewSet
 
 router = DefaultRouter()
 
 router.register(r'product-info', ResponseWithAllViewSet,
                 basename='product-info')
+router.register(r'action-info', ActionArticleViewSet, basename='action-info')
+
 
 urlpatterns = [
     path('', include(router.urls)),
--- kinmac/google_table/views.py
+++ kinmac/google_table/views.py
@@ -1,11 +1,41 @@
+from datetime import datetime
 import logging
-
-from rest_framework import viewsets
+from rest_framework import status, viewsets
 from rest_framework.response import Response
 
+from google_table.serializers import ActionArticlesSerializer
+from action.models import ArticleForAction
+from kinmac.constants_file import BRAND_LIST
 from google_table.logics import WbMarketplaceArticlesData
 
 logger = logging.getLogger(__name__)
+
+
+class ActionArticleViewSet(viewsets.ViewSet):
+    """ViewSet показывает в какой акции участвует артикул и какаяу него целевая цена"""
+    queryset = ArticleForAction.objects.all()
+    serializer_class = ActionArticlesSerializer
+
+    def list(self, request):
+        """Получеине данных а цене артикула в акции"""
+        try:
+            articles_for_actions = ArticleForAction.objects.select_related(
+                'action').filter(article__brand__in=BRAND_LIST, action__date_finish__gte=datetime.now()).order_by('article__common_article')
+            result = {}
+            for article_for_action in articles_for_actions:
+                action_name = article_for_action.action.name
+                date_start = article_for_action.action.date_start
+                date_finish = article_for_action.action.date_finish
+
+                if action_name not in result:
+                    result[action_name] = {
+                        'date_start': date_start, 'date_finish': date_finish, 'articles': []}
+                serializer = ActionArticlesSerializer(article_for_action)
+                result[action_name]['articles'].append(serializer.data)
+            return Response(result)
+        except Exception as e:
+            logger.error(f"Error occurred: {str(e)}")
+            return Response({'error': f'Произошла ошибка при получении данных.'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 class ResponseWithAllViewSet(viewsets.ViewSet):
