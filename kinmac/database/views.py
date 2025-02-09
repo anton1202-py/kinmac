f8ce8957c2ce05d42d40f5294828903e5bcc6942
anton1202-py <anton1202@yandex.ru>
2025-02-09 18:06:46 +0300

    [fix]: убрал выполнение функции с главной страницы

--- kinmac/database/views.py
+++ kinmac/database/views.py
@@ -11,7 +11,6 @@ from django.urls import reverse_lazy
 from django.views.generic import DeleteView, DetailView, ListView, UpdateView
 
 from kinmac.constants_file import BRAND_LIST
-from database.periodic_tasks import ozon_get_transactions_info
 
 from .forms import (
     ArticlesForm,
@@ -33,7 +32,6 @@ from .models import (
 def database_home(request):
     if str(request.user) == "AnonymousUser":
         return redirect("login")
-    ozon_get_transactions_info()
     data = Articles.objects.filter(brand__in=BRAND_LIST)
     context = {
         "data": data,
